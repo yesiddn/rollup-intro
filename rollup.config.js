6794b7f7e728dba59c582e4e4fefb6e1cae07892
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-03-10 13:11:51 -0500

    Add configuration for Rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,28 @@
+import html from '@rollup/plugin-html';
+import copy from 'rollup-plugin-copy';
+import css from 'rollup-plugin-css-only';
+import images from '@rollup/plugin-image';
+
 export default {
   input: 'src/index.js',
   output: {
     file: 'dist/bundle.js',
     format: 'es', // 'cjs', 'iife', 'umd'
     sourcemap: true
-  }
+  },
+  plugins: [
+    copy({
+      targets: [
+        {
+          src: 'src/assets/**/*', // copy all files from src/assets
+          dest: 'dist/assets' // destination folder
+        }
+      ]
+    }),
+    css({
+      output: 'bundle.css'
+    }),
+    html(), // no se pasan opciones porque se toman las opciones por defecto
+    images() // no se pasan opciones porque se toman las opciones por defecto
+  ]
 }
